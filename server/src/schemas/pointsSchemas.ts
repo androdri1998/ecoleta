ec906e6a202f6c2001a580efbd6e0766aca4fd20
André Rodrigues <andre.rodrigues98@outlook.com>
2020-06-04 01:28:42 -0300

    inserindo correção de schema

--- server/src/schemas/pointsSchemas.ts
+++ server/src/schemas/pointsSchemas.ts
@@ -8,8 +8,8 @@ export const createPoints = {
     whatsapp: Joi.string().min(1).required(),
     city: Joi.string().min(1).required(),
     uf: Joi.string().min(1).max(2).required(),
-    latitude: Joi.number().min(1).required(),
-    longitude: Joi.number().min(1).required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
     items: Joi.array().min(1).items(Joi.number()).required(),
   }),
 };
